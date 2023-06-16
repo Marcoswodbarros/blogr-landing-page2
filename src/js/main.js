28394b2beeb717e1ce2ea99b624b0edd1295a683
Marcos <marcoswodbarros@outlook.com>
2023-06-16 14:01:06 -0300

    Mobile header: Updaing submenu js

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,11 @@ let close = document.querySelector('#header__nav--close');
 let menu = document. querySelector('#header__mobile--menu');
 let connect = document.querySelector('#menu__li--connect');
 let subMenu = document.querySelector('#li__connect--ul');
+let arrowDown = document.querySelector('#li__connect--arrow');
 
 hamburguer.addEventListener("click", openMenu);
 close.addEventListener("click", closeMenu);
-connect.addEventListener("click", openSubMenu)
+arrowDown.addEventListener("click", openSubMenu)
 
 function openMenu () {
     hamburguer.style.display = "none";
@@ -18,12 +19,16 @@ function closeMenu () {
     hamburguer.style.display = "flex";
     close.style.display = "none";
     menu.style.display = "none";
+    subMenu.style.display = "none";
+    connect.style.color = "black";
 }
 
 function openSubMenu () {
-    subMenu.style.display = 'flex';
+    subMenu.style.display = "flex";
+    connect.style.color = "hsl(207, 13%, 34%)";
 }
 
 function closeSubMenu () {
-    subMenu.style.display = 'none';
+    subMenu.style.display = "none";
+    connect.style.color = "black";
 }
